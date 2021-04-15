eb6f30d35f0ab53fe062e6019ee06c239f8bc623
Thor Anderson <ta.anderson@gmail.com>
2021-04-15 15:28:20 -0600

    Fixed by Thor

--- Pokemon/pokecode.js
+++ Pokemon/pokecode.js
@@ -9,12 +9,23 @@ loadButton.addEventListener('click', () => {
 fetchButton.addEventListener('click', () => {
     getAPIData(`https://pokeapi.co/api/v2/pokemon/25`).then(
         (data) => {
-            populatePokeCard(data)
+            makeCard(data)
         }
     )
 })
 
-function fetchKantoPokemon() {
+async function getAPIData(url) {
+    try {
+        const response = await fetch(url) // try getting data from the API at the url
+        const data = await response.json() // convert the response into JSON
+        return data // return the data from the fuction to whoever called it
+    } catch (error) {
+        // must have been an error
+        console.log(error)
+    }
+}
+
+/* function fetchKantoPokemon() {
 
     fetch('https://pokeapi.co/api/v2/pokemon?limit=25')
 
@@ -26,6 +37,18 @@ function fetchKantoPokemon() {
             makeCard(pokemon)
         })
     })
+} */
+
+function loadPage() {
+    getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25&offset=748`).then(
+        async (data) => {
+            for (const singlePokemon of data.results) {
+                await getAPIData(singlePokemon.url).then(
+                    (pokeData) => makeCard(pokeData)
+                )
+            }
+        }
+    )
 }
 
 
@@ -35,8 +58,8 @@ function makeCard(data){
     let front = document.createElement('div')
     let back = document.createElement('div')
 
-    // let frontImage = document.createElement('img')
-    // frontImage.src = `images/${getImageFileName(pokemon)}.png`
+    let frontImage = document.createElement('img')
+    frontImage.src = getImageFileName(data)
 
     card.className = "card"
     content.className = "content"
@@ -47,25 +70,25 @@ function makeCard(data){
     let nameTag = document.createElement('p')
     nameTag.textContent = data.name
 
-    function loadPage() {
-        getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25&offset=748`).then(
-            async (data) => {
-                for (const singlePokemon of data.results) {
-                    await getAPIData(singlePokemon.url).then(
-                        (pokeData) => populatePokeCard(pokeData)
-                    )
-                }
-            }
-        )
-    }
-
 
     card.appendChild(content)
     content.appendChild(front)
     content.appendChild(back)
+    front.appendChild(frontImage)
     front.appendChild(nameTag)
     CARDCONTAINER.appendChild(card)
 
+}
+
+function getImageFileName(pokemon) {
+    let pokeId
+    if (pokemon.id < 10) pokeId = `00${pokemon.id}`
+    if (pokemon.id > 9 && pokemon.id < 100) pokeId = `0${pokemon.id}`
+    if (pokemon.id > 99 && pokemon.id < 810) pokeId = pokemon.id
+    if (pokemon.id === 900) {
+        return `images/pokeball.png`
+    }
+    return `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${pokeId}.png`
 }
 
 /* 
@@ -79,4 +102,4 @@ function makeCard(data){
     </div>
 </div>
 */
-fetchKantoPokemon()
+//fetchKantoPokemon()
