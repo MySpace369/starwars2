4330a1540c641db2827c3515f5040d058af3b3a2
Adrienne <10910835@my.uvu.edu>
2021-04-21 18:31:45 -0600

    added abilities and types

--- Pokemon/pokecode.js
+++ Pokemon/pokecode.js
@@ -9,7 +9,11 @@ class Pokemon {
         this.name = name
         this.height = height
         this.weight = weight
-        this.abilities = abilities
+        this.abilities = {
+            ability:{
+                name: abilities
+            }
+        }
         this.moves = moves
     }
 }
@@ -22,21 +26,24 @@ newButton.addEventListener('click', () => {
     let pokeName = prompt('What is the name of your new Pokemon?')
     let pokeHeight = prompt('What is the height of your Pokemon?')
     let pokeWeight = prompt('Pokemon weight?')
+    let abilites = prompt('Pokemon ability?')
+    
     let newPokemon = new Pokemon(
         pokeName,
         pokeHeight,
         pokeWeight,
-        ['eat', 'sleep'],
+        abilities,
         ['study', 'game']
     )
-     populatePokeCard(newPokemon)
+    //console.log(newPokemon)
+    makeCard(newPokemon)
 })
 
 fetchButton.addEventListener('click', () => {
     let pokeNameOrId = prompt("Enter Pokemon ID or Name:").toLowerCase()
-    console.log(pokeNameOrId)
+    //console.log(pokeNameOrId)
     getAPIData(`https://pokeapi.co/api/v2/pokemon/${pokeNameOrId}`).then(
-        (data) => populatePokeCard(data)
+        (data) => makeCard(data)
     )
 })
 
@@ -73,7 +80,7 @@ function loadPage() {
 fetchButton.addEventListener('click', () => {
     let pokeNameOrId = prompt("Enter Pokemon ID or Name:")
     // .toLowerCase()//to lower case of null
-    console.log(pokeNameOrId)
+    //console.log(pokeNameOrId)
     getAPIData(`https://pokeapi.co/api/v2/pokemon/${pokeNameOrId}`).then(
         (data) => makeCard(data)
     )
@@ -95,7 +102,28 @@ function populatePokeCard(singlePokemon) {
      pokeGrid.appendChild(pokeScene)
  }
 
+function abilities(array){
+    //console.log(array)
+    let abilityContatiner = document.createElement('div')
+    let abilityLabel = document.createElement('h1')
+    abilityLabel.textContent = "Abilites"
+    abilityContatiner.appendChild(abilityLabel)
+    abilityContatiner.className = 'ability_container'
+    for (let i = 0; i < array.length; i++) {
+        let abilityName = document.createElement('p')
+        abilityName.textContent = capitalize(array[i].ability.name)
+        //console.log(abilityName)
+        abilityContatiner.appendChild(abilityName)
+
+        
+    }
+    return abilityContatiner
+    //console.log(abilityContatiner)
+}
+
+
 function makeCard(data){
+    console.log(data.types[0].type.name)
     let card = document.createElement('div')
     let content = document.createElement('div')
     let front = document.createElement('div')
@@ -104,13 +132,18 @@ function makeCard(data){
     let frontImage = document.createElement('img')
     frontImage.src = getImageFileName(data)
     let title = document.createElement('p')
-    title.textContent = data.name
-    
+    title.textContent = capitalize(data.name)
     card.className = "card"
     content.className = "content"
     front.className = "front"
     back.className = "back"
 
+    ///// back card content ///////
+    setTypeStyle(data,front)
+    console.log(setTypeStyle(data,front))
+    
+    let abilityContainer = abilities(data.abilities)
+    //console.log(abilityContainer)
     ///// populate card data //////
     let nameTag = document.createElement('p')
     nameTag.textContent = data.name
@@ -119,6 +152,8 @@ function makeCard(data){
     card.appendChild(content)
     content.appendChild(front)
     content.appendChild(back)
+    back.appendChild(abilityContainer)
+
     front.appendChild(frontImage)
     front.appendChild(title)
     CARDCONTAINER.appendChild(card)
@@ -128,13 +163,18 @@ function makeCard(data){
     let pokeFront = document.createElement('div')
     pokeFront.className = 'card__face card__face--front'
     let frontLabel = document.createElement('p')
-    frontLabel.textContent = pokemon.name
+
+    
     let frontImage = document.createElement('img')
     frontImage.src = getImageFileName(pokemon)
 
     pokeFront.appendChild(frontLabel)
     pokeFront.appendChild(frontImage)
     return pokeFront
+}
+const capitalize = (s) => {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
 }
 
 function populateCardBack(pokemon) {
@@ -156,3 +196,57 @@ function getImageFileName(pokemon) {
     }
     return `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${pokeId}.png`
 }
+function setTypeStyle(data,element) {
+    let pokeTypes = []
+    let pokeType1 = data.types[0].type.name
+
+    if (data.types.length > 1) {
+        let pokeType2 = data.types[1].type.name
+        pokeTypes = [pokeType1,pokeType2]
+        console.log(pokeTypes)
+        element.style.setProperty(
+        'background',
+        `linear-gradient(${getPokeTypeColor(pokeType1)}, ${getPokeTypeColor(
+            pokeType2,
+        )})`,
+        )
+    } else {
+        element.style.setProperty('background', getPokeTypeColor(pokeType1))
+        pokeTypes = [pokeType1]
+    }
+    
+    return pokeTypes
+}
+
+function getPokeTypeColor(pokeType) {
+    let color
+    switch (pokeType) {
+      case 'grass':
+        color = '#0F0'
+        break
+      case 'fire':
+        color = '#F00'
+        break
+      case 'water':
+        color = '#00F'
+        break
+      case 'bug':
+        color = '#7fff00'
+        break
+      case 'normal':
+        color = '#f5f5dc'
+        break
+      case 'flying':
+        color = '#00ffff'
+        break
+      case 'poison':
+        color = '#c300ff'
+        break
+      case 'electric':
+        color = '#c8ff00'
+        break
+      default:
+        color = '#777'
+    }
+    return color
+  }
