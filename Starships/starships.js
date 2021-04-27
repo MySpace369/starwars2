94157088d88922f8d9634881235bf7d533073f92
Adrienne <10910835@my.uvu.edu>
2021-04-27 16:17:36 -0600

    Commented out starships and added css properties to congress page

--- Starships/starships.js
+++ Starships/starships.js
@@ -1,72 +1,72 @@
-import { starships } from '../data/starships.js'
-import { getLastNumber, removeChildren } from '../utils/index.js'
+// import { starships } from '../data/starships.js'
+// import { getLastNumber, removeChildren } from '../utils/index.js'
 
-const nav = document.querySelector('nav')
-const navList = document.getElementById("#navList") // see if thor can make this work
-const shipView = document.querySelector('.shipView')
-//console.log(navList)
-const dialog = document.querySelector('.modal')
-//console.log(dialog)
-const closeButton = document.querySelector('.modal-close')
-const modalBackground = document.querySelector('.modal-background')
+// const nav = document.querySelector('nav')
+// const navList = document.getElementById("#navList") // see if thor can make this work
+// const shipView = document.querySelector('.shipView')
+// //console.log(navList)
+// const dialog = document.querySelector('.modal')
+// //console.log(dialog)
+// const closeButton = document.querySelector('.modal-close')
+// const modalBackground = document.querySelector('.modal-background')
 
-closeButton.addEventListener('click', () => {
-    dialog.classList.toggle("is-active")
-})
+// closeButton.addEventListener('click', () => {
+//     dialog.classList.toggle("is-active")
+// })
 
-modalBackground.addEventListener('click', () => {
-    dialog.classList.toggle("is-active")
-})
+// modalBackground.addEventListener('click', () => {
+//     dialog.classList.toggle("is-active")
+// })
 
-function populateNav(starships) {
-    starships.forEach((starship) => {
-        let anchorWrap = document.createElement('a')
-        anchorWrap.href = '#'
-        anchorWrap.addEventListener('click', () => populateShipView(starship))
-        let listItem = document.createElement('li')
-        listItem.textContent = starship.name
+// function populateNav(starships) {
+//     starships.forEach((starship) => {
+//         let anchorWrap = document.createElement('a')
+//         anchorWrap.href = '#'
+//         anchorWrap.addEventListener('click', () => populateShipView(starship))
+//         let listItem = document.createElement('li')
+//         listItem.textContent = starship.name
 
-        anchorWrap.appendChild(listItem)
-        nav.appendChild(anchorWrap)
-    })
-}
+//         anchorWrap.appendChild(listItem)
+//         nav.appendChild(anchorWrap)
+//     })
+// }
 
-function populateShipView(shipData) {
-    removeChildren(shipView)
-    let shipNum = getLastNumber(shipData.url)
-    let shipImage = document.createElement('img')
-    shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
-    shipImage.addEventListener('error', (err) => {
+// function populateShipView(shipData) {
+//     removeChildren(shipView)
+//     let shipNum = getLastNumber(shipData.url)
+//     let shipImage = document.createElement('img')
+//     shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+//     shipImage.addEventListener('error', (err) => {
     
-        shipImage.hidden = true
-        dialog.classList.toggle("is-active")
-    })
-    shipView.appendChild(shipImage)
-}
+//         shipImage.hidden = true
+//         dialog.classList.toggle("is-active")
+//     })
+//     shipView.appendChild(shipImage)
+// }
 
-function addStarField(element, numStars) {
-    element.style.setProperty('background-color', '#000')
-    for (let i = 0; i < numStars; i++) {
-        let star = document.createElement('div')
-        star.style.setProperty('position', 'absolute')
-        star.style.setProperty('width', '2px')
-        star.style.setProperty('height', '2px')
-        star.style.setProperty('background-color', 'white')
-        let xy = getRandomPosition()
-        star.style.left = `${xy[0]}px`
-        star.style.top = `${ xy[1]}px`
-        element.appendChild(star)
-    }
-}
+// function addStarField(element, numStars) {
+//     element.style.setProperty('background-color', '#000')
+//     for (let i = 0; i < numStars; i++) {
+//         let star = document.createElement('div')
+//         star.style.setProperty('position', 'absolute')
+//         star.style.setProperty('width', '2px')
+//         star.style.setProperty('height', '2px')
+//         star.style.setProperty('background-color', 'white')
+//         let xy = getRandomPosition()
+//         star.style.left = `${xy[0]}px`
+//         star.style.top = `${ xy[1]}px`
+//         element.appendChild(star)
+//     }
+// }
 
-function getRandomPosition() {
-    let y = document.body.scrollHeight
-    let x = document.body.scrollWidth
-    let randomY = Math.floor(Math.random() * y)
-    let randomX = Math.floor(Math.random() * x)
-    return [randomX, randomY]
-}
+// function getRandomPosition() {
+//     let y = document.body.scrollHeight
+//     let x = document.body.scrollWidth
+//     let randomY = Math.floor(Math.random() * y)
+//     let randomX = Math.floor(Math.random() * x)
+//     return [randomX, randomY]
+// }
 
-populateNav(starships)
+// populateNav(starships)
 
-addStarField(document.querySelector('body'), 1000)
+// addStarField(document.querySelector('body'), 1000)
